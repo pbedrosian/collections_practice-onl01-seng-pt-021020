23d7bf4c21fff4f272094598f7cdc8d8059b8a42
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-02-20 02:24:02 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,16 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|x| x.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
