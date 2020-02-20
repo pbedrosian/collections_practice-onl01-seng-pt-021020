4bb796cd4955edad154c4350214ea07a64962e94
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-02-20 03:48:43 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,42 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|x| x.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each { |sym|  sym[2] = "$"}
+end
+
+def find_a(array)
+  array.select {|a| a.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject(0){|sum,x| sum + x }
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+  if index != 1
+    "#{element}s"
+  else
+     element
+  end
+end
+end
